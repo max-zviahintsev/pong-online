830dcd806e38a60915ce18807a70dbf341837320
Max Zviahintsev <max.zviahintsev@outlook.com>
2025-04-21 16:39:21 +0300

    Add fastify websocket

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,7 @@
 import { fastify, FastifyInstance } from 'fastify'
+import websocket from '@fastify/websocket'
 import { Server, IncomingMessage, ServerResponse } from 'node:http'
+import { wsTestRoute } from './routes/ws-test.route.ts'
 
 const app: FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify({
   logger: {
@@ -8,6 +10,11 @@ const app: FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify({
     },
   },
 })
+
+app.register(websocket)
+
+// ROUTES
+await app.register(wsTestRoute)
 
 app.get('/ping', async () => 'pong\n')
 
