351f0d3a6d29a9d283680643a9636f91313e2b10
Max Zviahintsev <max.zviahintsev@outlook.com>
2025-04-22 16:24:12 +0300

    Add CORS

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,21 @@
 /* eslint-disable no-console */
+import cors from '@fastify/cors'
 import app from './app.ts'
+
+await app.register(cors, {
+  origin: (origin, cb) => {
+    if (!origin) return cb(null, true)
+
+    const allowedOrigins = ['http://localhost:5173']
+
+    if (allowedOrigins.includes(origin)) {
+      cb(null, true)
+    } else {
+      cb(new Error('Not allowed by CORS'), false)
+    }
+  },
+  credentials: true,
+})
 
 const PORT = Number(process.env.PORT) || 8080
 const HOST = '0.0.0.0'
